30c7c98ad37628345d3948a212f87b2e11aa06fd
Khan, Farhan (E16657) <farhan.khan@cmegroup.com>
2016-01-20 21:57:48 -0600

    Adding algorithms for questions for strings

---
+++ java/arrays_and_strings/PalindromePermutation.java
@@ -0,0 +1,46 @@
+package arrays_and_strings;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * 1.4 Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or
+ * phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not
+ * need to be limited to just dictionary words.
+ */
+public class PalindromePermutation {
+	public static boolean isPermutationOfPalindrome(String str) {
+		PermutationCounter counter = new PermutationCounter();
+		str.chars().map(Character::toLowerCase).forEach(counter::update);
+		return counter.isOddCount();
+	}
+
+	/**
+	 * Executes test cases.
+	 *
+	 * @param args ignored
+	 */
+	public static void main(String[] args) {
+		assertTrue(isPermutationOfPalindrome("abccba"));
+		assertTrue(isPermutationOfPalindrome("a1bccba1"));
+		assertFalse(isPermutationOfPalindrome("a1bccba2"));
+		assertFalse(isPermutationOfPalindrome("abcdef"));
+	}
+
+	/**
+	 * Maintains the state of the counter. Thread-unsafe.
+	 */
+	private static class PermutationCounter {
+		private int[] charCount = new int[128];
+		private int oddCount = 0;
+
+		private void update(int ch) {
+			charCount[ch]++;
+			oddCount = charCount[ch] % 2 == 1 ? oddCount + 1 : oddCount - 1;
+		}
+
+		private boolean isOddCount() {
+			return oddCount <= 1;
+		}
+	}
+}
---
+++ java/arrays_and_strings/URLify.java
@@ -0,0 +1,34 @@
+package arrays_and_strings;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * 1.3 Write a method to replace all spaces in a string with'%20'. You may assume that the string has sufficient space
+ * at the end of the string to hold the additional characters, and that you are given the "true" length of the string.
+ * (Note: if implementing in Java, please use a character array so that you can perform this operation in place.)
+ * EXAMPLE
+ * Input: "Mr John Smith      ", 13
+ * Output: "Mr%20John%20Smith"
+ */
+public class URLify {
+	/**
+	 * Replaces all spaces with %20.
+	 *
+	 * @param urlString the string to update.
+	 */
+	public static String updateURL(String urlString) {
+		return urlString.chars().mapToObj(ch -> ch == ' ' ? "%20" : String.valueOf((char)ch))
+				.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+				.toString();
+	}
+
+	/**
+	 * Executes test cases.
+	 *
+	 * @param args ignored
+	 */
+	public static void main(String[] args) {
+		assertEquals("Mr%20John%20Doe", updateURL("Mr John Doe"));
+		assertEquals("Mr%20John%20Smith", updateURL("Mr John Smith"));
+	}
+}
