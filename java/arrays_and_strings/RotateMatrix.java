f15f259d181ad6075756526ec50f85ae9522c65a
Khan, Farhan <farhan_m_khan@yahoo.com>
2016-01-21 00:26:16 -0600

    Adding algorithms related to arrays and strings

---
+++ java/arrays_and_strings/RotateMatrix.java
@@ -0,0 +1,20 @@
+package arrays_and_strings;
+
+/**
+ * Given an image represented by an N*N matrix, where each pixel in the image is 4 bytes, write a method to rotate the
+ * image by 90 degrees. Can you do this in place?
+ */
+public class RotateMatrix {
+	public static void rotate(int[][] matrix) {
+
+	}
+
+	/**
+	 * Executes test cases.
+	 *
+	 * @param args ignored
+	 */
+	public static void main(String[] args) {
+
+	}
+}
---
+++ java/arrays_and_strings/StringCompression.java
@@ -0,0 +1,80 @@
+package arrays_and_strings;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * 1.6 Implement a method to perform basic string compression using the counts of repeated characters. For example, the
+ * string aabcccccaaa would become a2b1c5a3. If the "compressed" string would not become smaller than the original
+ * string, your method should return the original string. You can assume the string has only uppercase and lowercase
+ * letters (a-z).
+ */
+public class StringCompression {
+	/**
+	 * Compresses the given string.
+	 *
+	 * @param strToCompress the string to compress
+	 *
+	 * @return compressed string
+	 */
+	public static String compress(String strToCompress) {
+		String compressed = strToCompress.chars()
+				.collect(CompressedStringContainer::new, CompressedStringContainer::append,
+						CompressedStringContainer::combine).toString();
+
+		return compressed.length() <= strToCompress.length() ? compressed : strToCompress;
+	}
+
+	/**
+	 * Executes test cases.
+	 *
+	 * @param args ignored
+	 */
+	public static void main(String[] args) {
+		assertEquals("a5b1c5a3", compress("aaaaabcccccaaa"));
+		assertEquals("abc", compress("abc"));
+		assertEquals("", compress(""));
+	}
+
+	private static class CompressedStringContainer {
+		private StringBuilder stringBuilder = new StringBuilder();
+		private int count = -1;
+		private char currentChar;
+
+		private CompressedStringContainer append(int ch) {
+			if (count == -1) {
+				reset((char) ch);
+			} else if (currentChar != ch) {
+				append();
+				reset((char) ch);
+			} else {
+				count++;
+			}
+
+			return this;
+		}
+
+		private void reset(char ch) {
+			currentChar = ch;
+			count = 1;
+		}
+
+		private CompressedStringContainer combine(CompressedStringContainer compressedStringContainer) {
+			this.stringBuilder.append(compressedStringContainer.stringBuilder);
+			return this;
+		}
+
+		private void append() {
+			stringBuilder.append(currentChar);
+			stringBuilder.append(count);
+		}
+
+		@Override
+		public String toString() {
+			if (count != -1) {
+				append();
+			}
+
+			return stringBuilder.toString();
+		}
+	}
+}
